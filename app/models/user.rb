fc9e2787e50dd150c143c9b487306f0fafca330c
JuliCarracedo <julicarra@hotmail.com>
2021-06-08 16:11:36 -0300

    fix validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :username length:{maximum: 12}, presence: true, uniqueness: true
-    validates :age presence: true, greater_than_or_equal_to: 13
-    validates :email presence: true, format:{with: /@/}
+    validates :username, length:{maximum: 12}, presence: true, uniqueness: true
+    validates :age, presence: true, numericality:{greater_than_or_equal_to: 13}
+    validates :email, presence: true, format:{with: /@/}
 end
