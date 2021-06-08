d6e8e04ef680ae0678c4446c292d6cd22d2c8bdb
vicmaburrito <vmaldaraca@gmail.com>
2021-06-08 15:39:05 -0500

    Add post model

--- db/migrate/20210608174836_create_users.rb
+++ db/migrate/20210608174836_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :username, unique: true, presence: true
+      t.string :username
       t.string :email
       t.integer :age
 
